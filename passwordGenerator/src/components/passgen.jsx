cd6c8f27931bbe18db4b3d26edbd5c097ff43c84
Alkesh Nikalje <alkeshnikalje@gmail.com>
2023-09-20 16:22:44 +0530

    done with password generation functionality

--- passwordGenerator/src/components/passgen.jsx
+++ passwordGenerator/src/components/passgen.jsx
@@ -1,23 +1,78 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState, useRef } from "react";
 
 export default function Passgen() {
   const [len, setLen] = useState(8);
+  const [allowNumber, setAllowNumber] = useState(false);
+  const [allowChar, setAllowChar] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordRef = useRef(null);
+
+  const passGen = useCallback(() => {
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let pass = "";
+    if (allowNumber) str += "0123456789";
+    if (allowChar) str += "!@#$%^&*-_+=[]{}~`";
+
+    for (let i = 0; i < len; i++) {
+      const index = Math.floor(Math.random() * str.length);
+      pass += str.charAt(index);
+    }
+
+    setPassword(pass);
+  }, [len, allowChar, allowNumber]);
+
+  useEffect(() => {
+    passGen();
+  }, [len, allowChar, allowNumber]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
   return (
     <div className="main">
       <div className="inpandbtn">
         <h2 style={{ color: "white", marginRight: "10px" }}>
           Password Generator
         </h2>
-        <input type="text" style={{ width: "300px" }} />
-        <button style={{ marginLeft: "15px" }}>copy</button>
+        <input
+          type="text"
+          style={{ width: "300px" }}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          readOnly
+          ref={passwordRef}
+        />
+        <button
+          style={{ marginLeft: "15px" }}
+          onClick={copyPasswordToClipboard}
+        >
+          copy
+        </button>
       </div>
       <div className="inputs">
         <label>length: {len}</label>
-        <input type="range" min={6} max={100} />
+        <input
+          type="range"
+          min={6}
+          max={100}
+          onChange={(e) => setLen(Number(e.target.value))}
+        />
         <label>Numbers:</label>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={allowNumber}
+          onChange={() => setAllowNumber((allow) => !allow)}
+        />
         <label>Chars:</label>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={allowChar}
+          onChange={() => setAllowChar((allow) => !allow)}
+        />
       </div>
     </div>
   );
